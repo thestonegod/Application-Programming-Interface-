d63a97386a2213cc9f194c2ee707e5ed31062278
thestonegod <elshaddaisandstone@gmail.com>
2026-01-15 23:17:52 -0800

    Customized error message

--- firstapi.js
+++ firstapi.js
@@ -1,4 +1,11 @@
 //STONE GOD - First API FETCH
 fetch('https://pokeapi.co/api/v2/pokemon/raichu')
-    .then(response => console.log(response))
+//Customized error message
+    .then(response => {
+        if(!response.ok){
+            throw new Error("No El-shaddai, resource not found")
+        }
+     return response.json();
+    })
+    .then(data => console.log(data.id))
     .catch(error => console.error(error)); 
