37af0e6e7e6ab324e547694e83b2f5ec43653274
thestonegod <elshaddaisandstone@gmail.com>
2026-01-15 23:38:05 -0800

    Async, await

--- firstapi.js
+++ firstapi.js
@@ -7,5 +7,23 @@ fetch('https://pokeapi.co/api/v2/pokemon/raichu')
         }
      return response.json();
     })
-    .then(data => console.log(data.id))
+    .then(data => console.log(data))
     .catch(error => console.error(error)); 
+
+    fetData();
+
+    async function fetData() {
+
+        try{
+             const response = await fetch('https://pokeapi.co/api/v2/pokemon/typhlosion');
+
+             if(!response.ok){
+            throw new Error("Can't find such a resource");
+        }
+        
+        }
+        catch(error){
+            console.error(error);
+        }
+        
+    }
